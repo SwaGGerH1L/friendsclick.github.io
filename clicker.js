ab2fd94cbb0fea00f327546d83f92dada4936b81
R1L3Y 2G <63470553+SwaGGerH1L@users.noreply.github.com>
2024-06-01 16:33:39 +0300

    clicker.js

--- clicker.js
+++ clicker.js
@@ -4,3 +4,13 @@ document.getElementById('clickImage').addEventListener('click', function() {
     score++;
     document.getElementById('score').innerHTML = 'Score: ' + score + ' <img src="coin.png" alt="Coin">';
 });
+
+// Предотвращение увеличения страницы при двойном нажатии на мобильных устройствах
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function(event) {
+    let now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
